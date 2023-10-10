936b17565163b15d0a6a9e2bb9d9a989bb4d00e9
shahriar hasan <shahriar.hasan.1523@gmail.com>
2023-10-11 01:10:11 +0600

    initiating repo

---
+++ Quotes.js
@@ -0,0 +1,33 @@
+window.quotes = [
+  {
+    quote: "The only way to do great work is to love what you do.",
+    category: "Motivational",
+    author: "Steve Jobs",
+    addedBy: "Shariar-Hasan",
+  },
+  {
+    quote: "The best way to predict the future is to invent it.",
+    category: "Inspirational",
+    author: "Alan Kay",
+    addedBy: "Shariar-Hasan",
+  },
+  {
+    quote: "The only true wisdom is in knowing you know nothing.",
+    category: "Philosophical",
+    author: "Socrates",
+    addedBy: "Shariar-Hasan",
+  },
+  {
+    quote: "The only thing we have to fear is fear itself.",
+    category: "Historical",
+    author: "Franklin D. Roosevelt",
+    addedBy: "Shariar-Hasan",
+  },
+  {
+    quote:
+      "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
+    category: "Inspirational",
+    author: "Helen Keller",
+    addedBy: "Shariar-Hasan",
+  },
+];
