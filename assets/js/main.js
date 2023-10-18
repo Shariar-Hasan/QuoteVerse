9469675e1b7e6bfc728c7e76a91cd37696c184b9
Jenish Dhanani <202112106@daiict.ac.in>
2023-10-18 16:01:27 +0530

    Feat: Copy to Clipboard

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,3 +75,146 @@ document.addEventListener("DOMContentLoaded", () => {
   generateQuoteBtn.addEventListener("click", setQuote);
   selectedCategoryTag.addEventListener("change", setQuote);
 });
+
+
+// Copy to clipboard functionality
+const copyToClipboardButton = document.querySelector('.copy-to-clipboard');
+const copyIcon = document.getElementById('copy-icon');
+
+// Event listener for copy button click
+copyToClipboardButton.addEventListener('click', function () {
+  // Get quote body and author text
+  const quoteBody = document.querySelector('.quote .quote-text').innerHTML;
+  const quoteAuthor = document.querySelector('.quote .quote-author').innerHTML;
+  const text = quoteBody + quoteAuthor;
+
+  // Copy to clipboard and handle success or failure
+  copyToClipboard(text)
+    .then((successful) => {
+      // Replace icon class based on copy success
+      if (successful) {
+        replaceIconClass('fa-solid fa-check check-icon');
+      } else {
+        replaceIconClass('fa-solid fa-xmark cross-icon');
+      }
+
+      // Reset to copy icon after 0.5s
+      setTimeout(() => {
+        replaceIconClass('fa-regular fa-copy icon');
+      }, 800);
+    })
+    .catch((err) => {
+      console.error('Clipboard write permission request failed', err);
+    });
+});
+
+// Function to replace icon class with a fade effect
+function replaceIconClass(newClass) {
+  copyIcon.style.opacity = 0; // Set initial opacity to 0
+
+  setTimeout(function () {
+    copyIcon.className = `fa ${newClass} icon`;
+    copyIcon.style.opacity = 1; // Set opacity to 1 after changing the class
+  }, 250); // You can adjust the duration of the fade (in milliseconds) here
+}
+
+// Async function to copy text to clipboard
+async function copyToClipboard(text) {
+  // Check if the Clipboard API is supported
+  if (navigator.clipboard && window.isSecureContext) {
+    try {
+      await navigator.clipboard.writeText(text);
+      console.log('Text successfully copied to clipboard');
+      return true;
+    } catch (err) {
+      console.error('Unable to copy to clipboard', err);
+      return false;
+    }
+  } else {
+    // Fallback for browsers that do not support Clipboard API
+    return new Promise((resolve) => {
+      var textArea = document.createElement('textarea');
+      textArea.value = text;
+
+      // Make the textarea invisible
+      textArea.style.position = 'fixed';
+      textArea.style.top = 0;
+      textArea.style.left = 0;
+      textArea.style.width = '2em';
+      textArea.style.height = '2em';
+      textArea.style.padding = 0;
+      textArea.style.border = 'none';
+      textArea.style.outline = 'none';
+      textArea.style.boxShadow = 'none';
+      textArea.style.background = 'transparent';
+
+      document.body.appendChild(textArea);
+      textArea.select();
+
+      try {
+        // Execute copy command
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'Text successfully copied to clipboard' : 'Unable to copy to clipboard';
+        console.log(msg);
+        resolve(successful);
+      } catch (err) {
+        console.error('Unable to copy to clipboard', err);
+        resolve(false);
+      } finally {
+        document.body.removeChild(textArea);
+      }
+    });
+  }
+}
+
+// Function to display copy message tooltip
+function copyMessageTooltip(copyButton, copyButtonMessage) {
+  const tooltipVisibleTime = 2000; // How long to leave tooltip visible
+  const tooltipHideTime = 100; // Matches .inactive animation time
+
+  // Tooltip
+  const tooltip = document.getElementById('copy_tooltip');
+  tooltip.textContent = copyButtonMessage;
+  tooltip.classList.add('active');
+  copyButton.setAttribute('aria-describedby', 'copy_tooltip');
+
+  setTimeout(function () {
+    tooltip.classList.remove('active');
+    tooltip.classList.add('inactive');
+
+    // Create a clone of the tooltip to restart the animation
+    const newTooltip = tooltip.cloneNode(true);
+    tooltip.parentNode.replaceChild(newTooltip, tooltip);
+
+    copyButton.removeAttribute('aria-describedby');
+
+    setTimeout(function () {
+      newTooltip.classList.remove('inactive');
+      newTooltip.textContent = '';
+    }, tooltipHideTime);
+  }, tooltipVisibleTime);
+}
+
+// Event listener for quote section click to copy
+const quoteSection = document.querySelector('.quote')
+
+quoteSection.addEventListener('click', async function () {
+  // Get quote body and author text
+  const quoteBody = document.querySelector('.quote .quote-text').innerHTML;
+  const quoteAuthor = document.querySelector('.quote .quote-author').innerHTML;
+  const text = quoteBody + quoteAuthor;
+
+  // Copy to clipboard and display tooltip
+  await copyToClipboard(text)
+    .then((successful) => {
+      // Replace icon class based on copy success
+      if (successful) {
+        copyMessageTooltip(quoteSection, "Quote copied!");
+      } else {
+        copyMessageTooltip(quoteSection, "Quote copy failed!");
+      }
+    })
+    .catch((err) => {
+      console.error('Clipboard write permission request failed', err);
+    });
+});
