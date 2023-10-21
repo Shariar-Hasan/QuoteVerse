bb6b15b531f7ca8b5d543bdd37472d297550e3bb
Muhammad-Shahid-Jamal <shahid.jamal0343@gmail.com>
2023-10-22 01:46:35 +0500

    - [x] layout updated of contributor page - [x] hidden element created to store contributor card view - [x] script updated according to change in view

--- Contributors/script.js
+++ Contributors/script.js
@@ -1,29 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const contributorsContainer = document.getElementById('contributors-container');
+  const contributorsContainer = document.getElementById(
+    'contributors-container'
+  );
+  fetch('https://api.github.com/repos/Shariar-Hasan/QuoteVerse/contributors')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      data.forEach((contributor) => {
+        const contributor_card = getContributorCard();
+        contributor_card.href = contributor.html_url;
 
-    fetch('https://api.github.com/repos/Shariar-Hasan/QuoteVerse/contributors')
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(contributor => {
-                const contributorDiv = document.createElement('div');
-                contributorDiv.classList.add('contributor');
+        const img = contributor_card.querySelector('img');
+        img.src = contributor.avatar_url;
+        img.alt = contributor.login;
 
-                const contributorLink = document.createElement('a');
-                contributorLink.href = contributor.html_url;
-                contributorLink.target = '_blank';
-                contributorLink.rel = 'noopener noreferrer';
-
-                const img = document.createElement('img');
-                img.src = contributor.avatar_url;
-                img.alt = contributor.login;
-
-                const h3 = document.createElement('h3');
-                h3.textContent = contributor.login;
-
-                contributorLink.appendChild(img);
-                contributorLink.appendChild(h3);
-                contributorDiv.appendChild(contributorLink);
-                contributorsContainer.appendChild(contributorDiv);
-            });
-        });
+        contributor_card.querySelector('h4 span').textContent =
+          contributor.login;
+        contributorsContainer.appendChild(contributor_card);
+      });
+    });
 });
+
+function getContributorCard() {
+  return document.querySelector('.contributor-card').cloneNode(true);
+}
