62c3863fc8ebbc2a64bc806b9899efb809bce433
Yasar Labib <yasarlabib@gmail.com>
2023-10-27 09:37:49 -0500

    added 5 more quotes to Quotes.js

--- Quotes.js
+++ Quotes.js
@@ -35,8 +35,36 @@ window.quotes = [
     addedBy: "Kratin Aggarwal",
   },
 
-
-
+  {
+    quote: "We do not remember days, we remember moments.",
+    category: "Life",
+    author: "Cesare Pavese",
+    addedBy: "Yasar Labib",
+  },
+  {
+    quote: "A dead end is just a good place to turn around.",
+    category: "Life",
+    author: "Naomi Judd",
+    addedBy: "Yasar Labib",
+  },
+  {
+    quote: "The way I see it, if you want the rainbow, you gotta put up with the rain!",
+    category: "Life",
+    author: "Dolly Parton",
+    addedBy: "Yasar Labib",
+  },
+  {
+    quote: "Success is falling nine times and getting up ten.",
+    category: "Life",
+    author: "Jon Bon Jovi",
+    addedBy: "Yasar Labib",
+  },
+  {
+    quote: "Failure is the condiment that gives success its flavor.",
+    category: "Life",
+    author: "Truman Capote",
+    addedBy: "Yasar Labib",
+  },
 
   {
     quote: "No one can make you feel inferior without your consent.",
