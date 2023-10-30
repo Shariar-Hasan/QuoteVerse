f5e572a5fcc234b0d05a90997b785d0969a8b983
IbraheemHaseeb7 <ibraheemhaseeb7@gmail.com>
2023-10-30 12:34:25 +0500

    Added pagination for the contributors page

--- Contributors/script.js
+++ Contributors/script.js
@@ -2,10 +2,30 @@ document.addEventListener('DOMContentLoaded', () => {
   const contributorsContainer = document.getElementById(
     'contributors-container'
   );
-  fetch('https://api.github.com/repos/Shariar-Hasan/QuoteVerse/contributors')
+  const load_more_btn = document.getElementById("load-more")
+
+  let page_count = 1;
+  loadMore(page_count, contributorsContainer);
+
+  load_more_btn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    page_count++;
+    return loadMore(page_count, contributorsContainer, load_more_btn);
+  })
+});
+
+function loadMore(page_count, contributorsContainer, load_more_btn) {
+  fetch(`https://api.github.com/repos/Shariar-Hasan/QuoteVerse/contributors?per_page=30&page=${page_count}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+
+      if (data.length === 0) {
+          load_more_btn.classList.remove("active")
+          load_more_btn.classList.add("blocked")
+          return;
+      }
+
       data.forEach((contributor) => {
         const contributor_card = getContributorCard();
         contributor_card.href = contributor.html_url;
@@ -15,15 +35,15 @@ document.addEventListener('DOMContentLoaded', () => {
         img.alt = contributor.login;
 
         contributor_card.querySelector('.contributor-username').textContent = contributor.login;
-		if (contributor.contributions == 1)
-			contributor_card.querySelector(".contributor-commits").textContent = contributor.contributions + " commit";
-		else
-			contributor_card.querySelector(".contributor-commits").textContent = contributor.contributions + " commits";
-
+        if (contributor.contributions == 1) {
+          contributor_card.querySelector(".contributor-commits").textContent = contributor.contributions + " commit";
+        } else {
+          contributor_card.querySelector(".contributor-commits").textContent = contributor.contributions + " commits";
+        }
         contributorsContainer.appendChild(contributor_card);
       });
     });
-});
+}
 
 function getContributorCard() {
   return document.querySelector('.contributor-card').cloneNode(true);
